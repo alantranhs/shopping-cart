5ab77035134857acd491f62969f0e39aeb6e5e2c
Alan Tran <alantranhs@gmail.com>
2020-07-28 07:49:30 +0700

    add value Array

--- shopping-cart/src/components/shelf/filter/filter.js
+++ shopping-cart/src/components/shelf/filter/filter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import './style.scss'
@@ -10,18 +10,27 @@ let availableSizesChecked = availableSizes.map(size => ({ size, isChecked: false
 
 const Filter = () => {
   const [allSize, setAllSize] = React.useState(availableSizesChecked);
-  console.log(allSize, 'allSize');
+  const [selectedCheckboxes, setSelectedCheckboxes] = React.useState(new Set());
   const dispatch = useDispatch();
-  const newAllSize = [];
-  const handleSelectSize = (e) => {
-    dispatch(filter(availableSizesChecked));
+
+  const handleSelectSize = (label) => {
+    if (selectedCheckboxes.has(label)) {
+      selectedCheckboxes.delete(label);
+    } else {
+      selectedCheckboxes.add(label);
+    }
+
+    console.log(Array.from(selectedCheckboxes), 'Array.from(selectedCheckboxes)');
+
+    dispatch(filter(Array.from(selectedCheckboxes)));
   };
+
 
   return (
     <div className="filters">
       <h4 className="title">Sizes:</h4>
       {availableSizesChecked.map((availableSize, index) => (
-        <CheckBox handleSelectSize={handleSelectSize} ischecked={availableSizes.isChecked} label={availableSize.size} key={index}/>
+        <CheckBox handleSelectSize={() => handleSelectSize(availableSize.size)} ischecked={availableSizes.isChecked} label={availableSize.size} key={index}/>
       ))}
     </div>
   );
