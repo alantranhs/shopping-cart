e79aa735a376895722762dd51e0192374ba6dc44
Alan Tran <alantranhs@gmail.com>
2020-08-14 00:04:11 +0700

    add UI

--- shopping-cart/src/components/floatCart/index.js
+++ shopping-cart/src/components/floatCart/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './style.scss';
 
 const FloatCart = () => {
@@ -10,14 +10,14 @@ const FloatCart = () => {
 
   return (
     <div className={`float-cart ${openBox ? 'float-cart--open' : ''}`}>
-        <div className="float-cart__close-btn" onClick={handleOpenBox}>
-          X
-        </div>
-        ${!openBox &&
-          <span className="bag bag--float-cart-closed" onClick={handleOpenBox}>
+      <div className="float-cart__close-btn" onClick={handleOpenBox}>
+        X
+      </div>
+      ${!openBox &&
+    <span className="bag bag--float-cart-closed" onClick={handleOpenBox}>
             <span className="bag__quantity"></span>
           </span>
-        }
+    }
 
       <div className="float-cart__content">
         <div className="float-cart__header">
@@ -28,20 +28,31 @@ const FloatCart = () => {
         </div>
 
         <div className="float-cart__shelf-container">
-            <p className="shelf-empty">
-              Add some products in the bag <br />
-            </p>
+          <p className="shelf-empty">Add some products in the cart !</p>
+          <div className="shelf-item">
+            <div className="shelf-item__del"></div>
+            <div className="shelf-item__thumb"></div>
+            <div className="shelf-item__details">
+              <p className="title"> Sphynx Tie Dye Wine T-Shirt </p>
+              <p className="desc">
+                X | Front tie dye print<br/>Quantity: 5
+              </p>
+            </div>
+            <div className="shelf-item__price"><p>$ 9.00</p>
+              <div>
+                <button className="change-product-button">-</button>
+                <button className="change-product-button">+</button>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="float-cart__footer">
           <div className="sub">SUBTOTAL</div>
           <div className="sub-price">
-            <p className="sub-price__val">
-
-            </p>
+            <p className="sub-price__val">$ 27.00</p>
             <small className="sub-price__installment">
-                <span>
-                </span>
+              <span>OR UP TO 3 x $ 9.00</span>
             </small>
           </div>
           <div className="buy-btn">
@@ -50,7 +61,7 @@ const FloatCart = () => {
         </div>
       </div>
     </div>
-    )
+)
 };
 
 export default FloatCart
